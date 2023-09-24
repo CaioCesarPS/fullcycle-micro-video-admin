7b452622f71b2afd5abd3ef334b195d820976f6a
CaioCPS <caio.cesar@ducash.com.br>
2023-09-24 16:34:40 +0000

    feat: create category entity and create (factory) method

---
+++ src/domain/category.entity.ts
@@ -0,0 +1,32 @@
+export type CategoryConstructorProps = {
+  category_id?: string;
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+  created_at?: Date;
+};
+
+export type CategoryCreateCommand = {
+  name: string;
+};
+
+export class Category {
+  category_id?: string;
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+  created_at?: Date;
+
+  constructor(props: CategoryConstructorProps) {
+    this.category_id = props.category_id;
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.is_active = props.is_active ?? true;
+    this.created_at = props.created_at ?? new Date();
+  }
+
+  //this is a factory method
+  static create(props: CategoryCreateCommand) {
+    return new Category(props)
+  }
+}
